567f64088f8b42cada036137651cfc41994a748b
I4Yoavv <117881513+I4Yoavv@users.noreply.github.com>
2023-03-07 15:34:51 +0800

    Add files via upload

---
+++ lua/user/lsp/lsp-installer.lua
@@ -0,0 +1,61 @@
+local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not status_ok then
+  vim.notify("nvim-lspconfig not found!")
+  return
+end
+
+-- NOTE: 如果发现某些lsp server安装启动时出现， client exit x and signal 0 等错误
+-- 可能是因为node版本过低， 升级node版本即可
+-- 升级方法
+-- npm cache clean -f
+-- npm install -g n
+-- n stable
+
+-- Register a handler that will be called for all installed servers.
+-- Alternatively, you may also register handlers on specific server instances instead (see example below).
+lsp_installer.on_server_ready(function(server)
+  local opts = {
+    on_attach = require("user.lsp.handlers").on_attach,
+    capabilities = require("user.lsp.handlers").capabilities,
+    flags = {
+      debounce_text_changes = 150,
+    }
+  }
+
+  if server.name == "clangd" then
+    local clangd_opts = require("user.lsp.settings.clangd")
+    opts = vim.tbl_deep_extend("force", clangd_opts, opts)
+  end
+
+  if server.name == "jsonls" then
+    local jsonls_opts = require("user.lsp.settings.jsonls")
+    opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+  end
+
+  if server.name == "sumneko_lua" then
+    local sumneko_opts = require("user.lsp.settings.sumneko_lua")
+    opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+  end
+
+  if server.name == "pyright" then
+    local pyright_opts = require("user.lsp.settings.pyright")
+    opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+  end
+
+  if server == "rust_analyzer" then
+    local rust_opts = require "user.lsp.settings.rust"
+    opts = vim.tbl_deep_extend("force", rust_opts, opts)
+    --local rust_tools_status_ok, rust_tools = pcall(require, "rust-tools")
+    --if not rust_tools_status_ok then
+      --return
+    end
+
+    --rust_tools.setup(rust_opts)
+    --goto continue
+  --end
+
+
+  -- This setup() function is exactly the same as lspconfig's setup function.
+  -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+  server:setup(opts)
+end)
---
+++ lua/user/lsp/settings/pyright.lua
@@ -0,0 +1,26 @@
+return {
+	settings = {
+
+    python = {
+      analysis = {
+        typeCheckingMode = "off"
+      }
+    }
+	},
+}
+
+--return {
+  --cmd = { "py" },
+  --settings = {
+    --python = {
+      --analysis = {
+        --typeCheckingMode = "basic",
+        --diagnosticMode = "workspace",
+        --inlayHints = {
+          --variableTypes = true,
+          --functionReturnTypes = true,
+        --},
+      --},
+    --},
+  --},
+--}
